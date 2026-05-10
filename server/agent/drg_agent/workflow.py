5c3ef58762f501819e0018ff65abd883508cb122
zhegebi <2213665870@qq.com>
2026-05-10 17:18:44 +0800

    Add feature: drg_agent workflow

--- server/agent/drg_agent/workflow.py
+++ server/agent/drg_agent/workflow.py
@@ -1,8 +1,9 @@
 from openai import OpenAI
 from loguru import logger
 from pydantic import BaseModel
+from typing import Literal, List, Tuple
 
-from .models import MedicalRecord, NAME_TO_CODE, DIAG_TO_MDC, MDC_ADRG_DRG
+from .models import MedicalRecord, Complication, DrgResult, NAME_TO_CODE, DIAG_TO_MDC, MDC_ADRG_DRG, PROCEDURE_TO_ADRG, MCC_AND_CC
 from ...config import API_KEY
 
 
@@ -34,6 +35,7 @@ EXTRACT_MEDICAL_RECORD_SYSTEM_PROMPT = """
 7. 只输出JSON，不要输出任何其他文本或解释。
 """
 def extract_medical_record_info(medical_record_text: str) -> MedicalRecord:
+    logger.info("extracting medical record info from text")
     # 1. call api
     try:
         response = client.chat.completions.create(
@@ -46,18 +48,18 @@ def extract_medical_record_info(medical_record_text: str) -> MedicalRecord:
             response_format={"type": "json_object"}
         )
     except Exception as e:
-        logger.error(f"Error calling DeepSeek API: {e}")
-        raise e
+        logger.error(f"Error calling DeepSeek API on extract medical record info: {e}")
+        raise RuntimeError(f"Error calling DeepSeek API on extract medical record info: {e}")
     # 2. parse response
     try:
         content = response.choices[0].message.content
         if content is None:
-            raise ValueError("Empty response from DeepSeek API")
+            raise ValueError("Empty response from DeepSeek API on extract medical record info")
         medical_record = MedicalRecord.model_validate_json(content)
         return medical_record
     except Exception as e:
-        logger.error(f"Error parsing JSON response: {e}")
-        raise e
+        logger.error(f"Wrong medical record info format: {e}")
+        raise RuntimeError(f"Wrong medical record info format: {e}")
 
 # step 2: get MDC code
 GET_MDC_SYSTEM_PROMPT = f"""
@@ -74,19 +76,20 @@ MDC代码及其名称定义如下：
    只输出这个JSON对象，不要有任何额外文字。
 4. 如果病例信息不足以明确区分，选择最接近的候选。
 """
-def get_mdc(medical_record_text:str, medical_record: MedicalRecord) -> str:
-    # get primary diagnosis code
+def get_mdc_code(medical_record_text:str, medical_record: MedicalRecord) -> str:
+    logger.info("getting MDC code from medical record")
+    # 1. get primary diagnosis code
     primary_diagnosis_code = medical_record.primary_diagnosis.code
     if primary_diagnosis_code is None:
         primary_diagnosis_code = NAME_TO_CODE.diagnosis.get(medical_record.primary_diagnosis.name, None)
     if primary_diagnosis_code is None:
         logger.error(f"unknown diagnosis {medical_record.primary_diagnosis.name}")
         raise ValueError(f"unknown diagnosis {medical_record.primary_diagnosis.name}")
-    # get MDC code
+    # 2. get MDC code
     mdc_code_list = DIAG_TO_MDC.data.get(primary_diagnosis_code, None)
     if mdc_code_list is None or len(mdc_code_list) == 0:
         logger.error(f"cannot find MDC code for diagnosis {medical_record.primary_diagnosis.name}")
-        raise ValueError(f"cannot find MDC code for diagnosis {medical_record.primary_diagnosis.name}")
+        raise RuntimeError(f"cannot find MDC code for diagnosis {medical_record.primary_diagnosis.name}")
     if len(mdc_code_list) == 1:
         return mdc_code_list[0]
     else:
@@ -125,4 +128,131 @@ def get_mdc(medical_record_text:str, medical_record: MedicalRecord) -> str:
                 return mdc_code_list[0]
         except Exception as e:
             logger.error(f"Error getting MDC code: {e}")
-            raise e
+            raise RuntimeError(f"Error getting MDC code: {e}")
+
+# step 3: get ADRG code
+def get_adrg_code(medical_record: MedicalRecord, mdc_code: str) -> str:
+    logger.info("getting ADRG code from medical record")
+    # 1.get primary procedure code
+    primary_procedure_code = medical_record.primary_procedure.code
+    if primary_procedure_code is None:
+        primary_procedure_code = NAME_TO_CODE.procedure.get(medical_record.primary_procedure.name, None)
+    if primary_procedure_code is None:
+        logger.error(f"unknown procedure {medical_record.primary_procedure.name}")
+        raise ValueError(f"unknown procedure {medical_record.primary_procedure.name}")
+    # 2. get ADRG code
+    adrg_code_list = PROCEDURE_TO_ADRG.data.get(primary_procedure_code, None)
+    if adrg_code_list is None or len(adrg_code_list) == 0:
+        logger.error(f"cannot find ADRG code for procedure {primary_procedure_code}")
+        raise RuntimeError(f"cannot find ADRG code for procedure {primary_procedure_code}")
+    for result in adrg_code_list:
+        if result.mdc_code == mdc_code:
+            return result.adrg_code
+    logger.error(f"cannot find ADRG code for MDC {mdc_code} and procedure {primary_procedure_code}")
+    raise RuntimeError(f"cannot find ADRG code for MDC {mdc_code} and procedure {primary_procedure_code}")
+
+# step 4: get MCC and CC level
+def get_mcc_cc_level(
+    medical_record: MedicalRecord
+) -> Literal[Complication.CC, Complication.MCC, Complication.NO]:
+    logger.info("getting MCC and CC level from medical record")
+    # 1. get primary diagnosis code
+    primary_diagnosis_code = medical_record.primary_diagnosis.code
+    if primary_diagnosis_code is None:
+        primary_diagnosis_code = NAME_TO_CODE.diagnosis.get(medical_record.primary_diagnosis.name, None)
+    if primary_diagnosis_code is None:
+        logger.error(f"unknown diagnosis {medical_record.primary_diagnosis.name}")
+        raise ValueError(f"unknown diagnosis {medical_record.primary_diagnosis.name}")
+    # 2. get secondary diagnosis code list
+    secondary_diagnosis_list = medical_record.secondary_diagnosis_list
+    secondary_diagnosis_code_list: List[str] = []
+    for diagnosis in secondary_diagnosis_list:
+        diagnosis_code = diagnosis.code
+        if diagnosis_code is None:
+            diagnosis_code = NAME_TO_CODE.diagnosis.get(diagnosis.name, None)
+        if diagnosis_code is None:
+            logger.warning(f"unknown diagnosis {diagnosis.name} on secondary diagnosis list, skip")
+        else:
+            secondary_diagnosis_code_list.append(diagnosis_code)
+    if len(secondary_diagnosis_code_list) == 0:
+        return Complication.NO
+    # 3. judge MCC and CC
+    exclusion_tables = MCC_AND_CC.exclusion_tables
+    mcc_list = MCC_AND_CC.mcc
+    cc_list = MCC_AND_CC.cc
+    for code in secondary_diagnosis_code_list:
+        for mcc_diag in mcc_list:
+            if mcc_diag.code == code:
+                exclusion_table_name = mcc_diag.exclusion_table
+                exclusion_table = exclusion_tables.get(exclusion_table_name, None)
+                if exclusion_table is None or primary_diagnosis_code not in exclusion_table:
+                    return Complication.MCC
+                break
+        for cc_diag in cc_list:
+            if cc_diag.code == code:
+                exclusion_table_name = cc_diag.exclusion_table
+                exclusion_table = exclusion_tables.get(exclusion_table_name, None)
+                if exclusion_table is None or primary_diagnosis_code not in exclusion_table:
+                    return Complication.CC
+                break
+    return Complication.NO
+
+# step 5: get DRG code and name
+def get_drg_code_and_name( 
+    adrg_code: str, 
+    mcc_cc_level: Literal[Complication.CC, Complication.MCC, Complication.NO]
+) -> Tuple[str, str]:
+    """
+    return (drg_code, drg_name)
+    """
+    logger.info("getting DRG code from medical record")
+    # 1. get DRG code list
+    drg_list = MDC_ADRG_DRG.drg.get(adrg_code, None)
+    if drg_list is None:
+        logger.error(f"cannot find DRG code list for ADRG {adrg_code}")
+        raise RuntimeError(f"cannot find DRG code list for ADRG {adrg_code}")
+    # 2. select DRG code
+    for drg in drg_list:
+        if drg.type == Complication.ANY:
+            return (drg.code, drg.name)
+        if drg.type == Complication.CC_or_MCC and mcc_cc_level != Complication.NO:
+            return (drg.code, drg.name)
+        if drg.type == mcc_cc_level:
+            return (drg.code, drg.name)
+    logger.error(f"cannot find DRG code for ADRG {adrg_code} and MCC and CC level {mcc_cc_level}")
+    raise RuntimeError(f"cannot find DRG code for ADRG {adrg_code} and MCC and CC level {mcc_cc_level}")
+
+# step 6: final result
+def get_final_result(
+    medical_record: MedicalRecord,
+    mdc_code: str,
+    adrg_code: str,
+    mcc_cc_level: Literal[Complication.CC, Complication.MCC, Complication.NO],
+    drg_code: str,
+    drg_name: str
+) -> DrgResult:
+    logger.info("getting final result")
+    # 1. get MDC name
+    mdc_name = MDC_ADRG_DRG.mdc.get(mdc_code, None)
+    if mdc_name is None:
+        logger.error(f"unknown MDC {mdc_code}")
+        raise ValueError(f"unknown MDC {mdc_code}")
+    # 2. get ADRG name
+    adrg_name = MDC_ADRG_DRG.adrg.get(adrg_code, None)
+    if adrg_name is None:
+        logger.error(f"unknown ADRG {adrg_code}")
+        raise ValueError(f"unknown ADRG {adrg_code}")
+    # 3. get reason for the result
+    lines = [
+        f"主诊断 {medical_record.primary_diagnosis.name} 匹配到的MDC为 {mdc_code}（{mdc_name}）",
+        f"手术 {medical_record.primary_procedure.name} 匹配到的ADRG为 {adrg_code}（{adrg_name}）",
+        f"次诊断 {','.join([f'{diagnosis.name}' for diagnosis in medical_record.secondary_diagnosis_list])} 由此判定并发症等级为 {mcc_cc_level.value}",
+        f"根据并发症等级 {mcc_cc_level.value} 选择DRG为 {drg_code}（{drg_name}）"
+    ]
+    return DrgResult(
+        mdc=mdc_code,
+        adrg=adrg_code,
+        drg=drg_code,
+        complication=mcc_cc_level,
+        reason="\n".join(lines)
+    )
