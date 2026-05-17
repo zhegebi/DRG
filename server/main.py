c07fe17c41706b5d27a13a7ed6bafd32cbdec44c
zhegebi <2213665870@qq.com>
2026-05-17 22:01:07 +0800

    Rollback backend codes

--- server/main.py
+++ server/main.py
@@ -1,19 +1,17 @@
 from contextlib import asynccontextmanager
-import asyncio
 
 from fastapi import FastAPI
 
+from .agent.docgen_agent.api import router as agent_router
+from .agent.drg_agent.api import router as drg_agent_router
 from .db import init_db
 from .static import init_assets, mount_static
 from .user.api import router as auth_router
-from .agent.docgen_agent.api import router as agent_router
-from .agent.drg_agent.api import router as drg_agent_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # 在异步环境中执行同步的 init_db
-    await asyncio.to_thread(init_db)
+    init_db()
     init_assets()
     yield
     # nothing to clean up on shutdown for now
